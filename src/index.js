e8b8817abb7e5eb6f5cfec0e589c8fff7c4f5097
Никита Гавшин <git add>
2020-10-01 12:40:57 +0400

    feat: start work typ-arr-prob

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+    let min = 0;
+    if (!array || !array.length) return min;
+    for (let i = 0; i < array.length; i++) {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function getMaxSubSum(array) {
+    let max = 0;
+    if (!array || !array.length) return max;
+    for (let i = 0; i < array.length; i++) {
+        if (max < array[i]) max = array[i];
+    }
+    return max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let sumArr = 0;
+    if (!array || !array.length) return sumArr;
+    for (let i = 0; i < array.length; i++) {
+        sumArr += array[i];
+    }
+    return sumArr / array.length;
+};
